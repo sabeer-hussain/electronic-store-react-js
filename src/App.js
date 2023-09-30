a10ec83b7a939785926902554894f18d0afe5f8b
sabeer-hussain <msabeerhussain.edu@gmail.com>
2023-09-30 17:12:52 +0530

    Adding Page Navigation in React using React Router DOM

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import { Button } from "react-bootstrap";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Index from "./pages";
+import About from "./pages/about";
+import Services from "./pages/services";
+import Cart from "./pages/cart";
+import Store from "./pages/store";
+import Dashboard from "./pages/users/dashboard";
+import Profile from "./pages/users/profile";
+import AboutUser from "./pages/users/about-user";
+import CustomNavbar from "./components/CustomNavbar";
 
 function App() {
   return (
-    <div>
-      <Button variant="success">Hey this is my first button</Button>
-    </div>
+    // setting up routes
+
+    <BrowserRouter>
+      <CustomNavbar />
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/users" element={<Dashboard />}>
+          <Route path="profile" element={<Profile />} />
+          <Route path="about" element={<AboutUser />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
