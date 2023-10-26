f04f25db9cfd7e820fa1892894a0bacaa35e96af
sabeer-hussain <msabeerhussain.edu@gmail.com>
2023-10-26 20:47:40 +0530

    Finishing Cart Design

--- src/components/users/SingleCartItemView.jsx
+++ src/components/users/SingleCartItemView.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Card, Col, Row } from "react-bootstrap";
+import { Button, Card, Col, Row } from "react-bootstrap";
 import { getProductImage } from "../../services/ProductService";
 import defaultProductImage from "../../assets/default_product_image.jpg";
 
@@ -32,7 +32,7 @@ const SingleCartItemView = ({ item }) => {
               }}
             />
           </Col>
-          <Col md={10}>
+          <Col md={9}>
             <h5>{item.product.title}</h5>
             <p className="text-muted">
               <span>
@@ -61,7 +61,32 @@ const SingleCartItemView = ({ item }) => {
               </Col>
             </Row>
           </Col>
-          <Col md={3}></Col>
+          <Col
+            md={2}
+            className="d-flex align-items-center justify-content-center"
+          >
+            <div className="w-100">
+              <div className="d-grid">
+                <Button variant="danger" size="sm">
+                  Remove
+                </Button>
+              </div>
+              <div className="mt-2">
+                <Row>
+                  <Col className="d-grid">
+                    <Button variant="info" size="sm">
+                      -
+                    </Button>
+                  </Col>
+                  <Col className="d-grid">
+                    <Button variant="success" size="sm">
+                      +
+                    </Button>
+                  </Col>
+                </Row>
+              </div>
+            </div>
+          </Col>
         </Row>
       </Card.Body>
     </Card>
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,10 +1,18 @@
 import { useContext } from "react";
 import CartContext from "../context/CartContext";
-import { Card, Col, Container, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import SingleCartItemView from "../components/users/SingleCartItemView";
 
 function Cart() {
   const { cart, setCart, addItem, removeItem } = useContext(CartContext);
+
+  const getTotalCartAmount = () => {
+    let amount = 0;
+    cart.items.forEach((item) => {
+      amount += item.totalPrice;
+    });
+    return amount;
+  };
 
   const cartView = () => {
     return (
@@ -26,6 +34,14 @@ function Cart() {
                 ))}
               </Col>
             </Row>
+            <Container className="px-5">
+              <h3 className="text-end px-5">
+                Total Amount : ₹ {getTotalCartAmount()}
+              </h3>
+            </Container>
+            <Container className="text-center">
+              <Button size="lg">Place Order</Button>
+            </Container>
           </Card.Body>
         </Card>
       </>
