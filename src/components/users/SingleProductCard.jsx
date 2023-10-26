168992f65ee3acf05032add6d75c404fad0b696f
sabeer-hussain <78043020+sabeer-hussain@users.noreply.github.com>
2023-10-26 09:06:01 +0530

    Es 31/work on store and category for guest user (#17) (#18)
    * Es 20/project setup frontend (#1)
    
    * Es 21/design registration page (#3)
     
    * Es 22/design login page (#5)
    
    * Es 23/create user profile management (#7)
    
    * Es 25/work on sidenav and categories for admin module (#9)
    
    * Es 26/work on products for admin module (#11)
    
    * Es 28/manage orders in admin module (#13)
    
    * Es 30/manage users in admin module (#15)
    
    * Es 31/work on store and category for guest user (#17)

---
+++ src/components/users/SingleProductCard.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import { Badge, Button, Card, Container } from "react-bootstrap";
+import defaultProductImage from "../../assets/default_product_image.jpg";
+import { getProductImage } from "../../services/ProductService";
+import "./SingleProductCard.css";
+import { Link } from "react-router-dom";
+
+const SingleProductCard = ({ product }) => {
+  const [productImage, setProductImage] = useState(undefined);
+
+  useEffect(() => {
+    if (product) {
+      getProductImageFromServer();
+    }
+  }, [product]);
+
+  const getProductImageFromServer = async () => {
+    // api call
+    setProductImage(await getProductImage(product.productId));
+  };
+
+  return (
+    <Card className="m-1 shadow-sm">
+      <Card.Body>
+        <Container className="text-center">
+          <img
+            src={productImage}
+            alt={product.title}
+            onError={(event) => {
+              event.currentTarget.setAttribute("src", defaultProductImage);
+            }}
+            className="product-image"
+          />
+        </Container>
+        <h6>{product.title}</h6>
+        <p className="text-muted">
+          Short description
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, natus?
+          </span>
+        </p>
+        <Badge pill bg="info">
+          {product.category?.title}
+        </Badge>
+        <Badge className="ms-2" pill bg={product.stock ? "success" : "danger"}>
+          {product.stock ? "In Stock" : "Out of Stock"}
+        </Badge>
+        <Container className="text-end">
+          <b>
+            <span className="h3 text-muted">
+              <s>₹{product.price}</s>
+            </span>
+          </b>
+          <b>
+            <span className="h4 ms-2">₹{product.discountedPrice}</span>
+          </b>
+        </Container>
+        <Container className="d-grid mt-4">
+          <Button
+            as={Link}
+            to={`/store/products/${product.productId}`}
+            variant="success"
+            size="sm"
+          >
+            View Product
+          </Button>
+        </Container>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default SingleProductCard;
