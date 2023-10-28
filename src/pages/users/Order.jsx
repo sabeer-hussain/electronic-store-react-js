436803c85ddae7d6d1924b1998a2d760e1b19d49
sabeer-hussain <msabeerhussain.edu@gmail.com>
2023-10-28 14:18:11 +0530

    Working on Users Orders page

--- src/pages/users/Order.jsx
+++ src/pages/users/Order.jsx
@@ -1,5 +1,28 @@
+import { useContext, useEffect, useState } from "react";
+import { getOrdersOfUser } from "../../services/OrderService";
+import UserContext from "../../context/UserContext";
+import { toast } from "react-toastify";
+
 const Order = () => {
-  return <h1>This is order page</h1>;
+  const { userData, isLogin } = useContext(UserContext);
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    loadOrdersOfUser();
+  }, []);
+
+  const loadOrdersOfUser = async () => {
+    try {
+      const result = await getOrdersOfUser(userData.user.userId);
+      console.log(result);
+      setOrders(result);
+    } catch (error) {
+      console.log(error);
+      toast.error("Error in loading orders");
+    }
+  };
+
+  return <h1>This is order</h1>;
 };
 
 export default Order;
